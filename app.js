8629587b16faf009784882d4761fd9acba869c14
jesushilarioh <jesushilariohernandez@gmail.com>
2018-07-29 01:59:26 -0500

    deleted

--- app.js
+++
@@ -1,7 +0,0 @@
-const math_functions = require("./math_functions.js");
-
-console.log(math_functions.addition(4, 4));
-console.log(math_functions.subtraction(4, 5));
-console.log(math_functions.multiplication(4, 9));
-console.log(math_functions.division(6, 0));
-console.log(math_functions.division(6, 6));
