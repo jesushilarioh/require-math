36bf2b07dda0209438879b4a83ec475def078a31
jesushilarioh <jesushilariohernandez@gmail.com>
2018-07-31 22:31:36 -0500

    added

--- 2D-shapes.js
+++ 2D-shapes.js
@@ -1,33 +1,35 @@
 // 2D-shapes
-module.exports.Annulus = {
-  Area = {
-    inner_outer_radius_defined(r, R) {
+// 2D-shapes
+// 2D-shapes
+exports.Annulus = {
+  Area: {
+    inner_outer_radius_defined: inner_outer_radius_defined = function(r, R) {
       return Math.PI * (Math.pow(R, 2) - Math.pow(r, 2));
     },
-    average_radius_width_defined(p, w) {
+    average_radius_width_defined: average_radius_width_defined = function(p, w) {
       return 2 * Math.PI * p * w;
     }
   },
-  Average_radius(r, R) {
+  Average_radius: Average_radius = function(r, R) {
     return .5 * (r + R);
   },
-  Width(r, R) {
+  Width: Width = function(r, R) {
     return R - r;
   }
 };
-module.exports.Circle = {
-  Area(r) {
+exports.Circle = {
+  Area: Area = function(r) {
     return Math.PI * Math.pow(r, 2);
   },
-  Circumference = {
-    diameter_defined(d) {
+  Circumference: {
+    diameter_defined: diameter_defined = function(d) {
       return Math.PI * d;
     },
-    radius_defined(r) {
+    radius_defined: radius_defined = function(r) {
       return 2 * Math.PI * r;
     }
   },
-  Diameter(r){
+  Diameter: Diameter = function(r){
     return 2 * r;
   } 
 };
